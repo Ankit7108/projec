b480badaddefa9f1c4e28f2fd7cbb59c3be897d6
Ankit Choudhary <126074400+Ankit7108@users.noreply.github.com>
2026-03-03 02:27:59 +0530

    Update

---
+++ test.py
@@ -0,0 +1,12 @@
+def calculate_sum(a, b)
+print("Calculating sum...")  
+    result = a + b
+    return reslt 
+
+
+    if __name__ == "__main__":
+    number1 = 10
+    number2 = 5
+
+    total = calculate_summ(number1, number2)
+    print("Total is:", total)
